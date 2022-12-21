359281d18b2b8762da54c2c642ab154ab523ab84
kyfuh <kylefuhrmann1@gmail.com>
2022-12-20 16:26:19 -0800

    oops forgot this

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -65,4 +65,5 @@ class ReStructuredTextParser(Parser):
 
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
-        
+
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
